97c1d2c9b3d05a54bb192cb2a0fb0e36bef98432
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-01-20 20:15:32 -0330

    Factor out ESLint config

--- index.test.ts
+++ index.test.ts
@@ -1,16 +1,16 @@
 import SafeEventEmitter from './index';
 
-describe('SafeEventEmitter', () => {
-  test('can be constructed without error', () => {
+describe('safeEventEmitter', () => {
+  it('can be constructed without error', () => {
     expect(new SafeEventEmitter()).toBeDefined();
   });
 
-  test('can emit a value with no listeners', () => {
+  it('can emit a value with no listeners', () => {
     const see = new SafeEventEmitter();
     expect(see.emit('foo', 42)).toBe(false);
   });
 
-  test('can emit a value with 1 listeners', () => {
+  it('can emit a value with 1 listeners', () => {
     expect.assertions(2);
 
     const see = new SafeEventEmitter();
@@ -18,7 +18,7 @@ describe('SafeEventEmitter', () => {
     expect(see.emit('foo', 42)).toBe(true);
   });
 
-  test('can emit a value with 2 listeners', () => {
+  it('can emit a value with 2 listeners', () => {
     expect.assertions(3);
 
     const see = new SafeEventEmitter();
@@ -27,14 +27,14 @@ describe('SafeEventEmitter', () => {
     expect(see.emit('foo', 42)).toBe(true);
   });
 
-  test('returns false when _events is somehow undefined', () => {
+  it('returns false when _events is somehow undefined', () => {
     const see = new SafeEventEmitter();
     see.on('foo', () => { /* */ });
     delete (see as any)._events;
     expect(see.emit('foo', 42)).toBe(false);
   });
 
-  test('throws error from handler after setTimeout', () => {
+  it('throws error from handler after setTimeout', () => {
     jest.useFakeTimers();
     const see = new SafeEventEmitter();
     see.on('boom', () => {
@@ -48,14 +48,14 @@ describe('SafeEventEmitter', () => {
     }).toThrow('foo');
   });
 
-  test('throws error emitted when there is no error handler', () => {
+  it('throws error emitted when there is no error handler', () => {
     const see = new SafeEventEmitter();
     expect(() => {
       see.emit('error', new Error('foo'));
     }).toThrow('foo');
   });
 
-  test('throws error emitted when there is no error handler AND _events is somehow undefined', () => {
+  it('throws error emitted when there is no error handler AND _events is somehow undefined', () => {
     const see = new SafeEventEmitter();
     delete (see as any)._events;
     expect(() => {
@@ -63,14 +63,14 @@ describe('SafeEventEmitter', () => {
     }).toThrow('foo');
   });
 
-  test('throws default error when there is no error handler and error event emitted', () => {
+  it('throws default error when there is no error handler and error event emitted', () => {
     const see = new SafeEventEmitter();
     expect(() => {
       see.emit('error');
     }).toThrow('Unhandled error.');
   });
 
-  test('throws error when there is no error handler and error event emitted', () => {
+  it('throws error when there is no error handler and error event emitted', () => {
     const see = new SafeEventEmitter();
     expect(() => {
       see.emit('error', { message: 'foo' });
--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-/* global Reflect */
 import { EventEmitter } from 'events';
 
 type Handler = (...args: any[]) => void;
