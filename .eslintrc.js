9a40331048258b5ae2295b783961525118182317
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-02-12 16:47:58 -0330

    Update ESLint config to override env for JS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     '@metamask/eslint-config',
     '@metamask/eslint-config/config/jest',
+    '@metamask/eslint-config/config/nodejs',
     '@metamask/eslint-config/config/typescript',
   ],
 };
